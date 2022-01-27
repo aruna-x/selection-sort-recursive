9926a232410260ad84aa9d0b1d180e6a82f10352
Aruna <aruna.evan15@gmail.com>
2022-01-27 10:22:30 -0800

    Solution

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -1,16 +1,54 @@
 function selectionSortRecursive(arr) {
-  // type your code here
+  switch(arr.length){
+    case 0:
+      return [];
+    case 1:
+      return arr;
+    default:
+      const min = Math.min(...arr);
+      const ind = arr.indexOf(min);
+      arr.splice(ind,1)
+      return [min, ...selectionSortRecursive(arr)];
+  }
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
+
+  console.log("");
+
+  console.log("Expecting: [5]");
+  console.log("=>", selectionSortRecursive([5]));
+
+  console.log("");
+
+  console.log("Expecting: [1, 1]");
+  console.log("=>", selectionSortRecursive([1, 1]));
+
+  console.log("");
+
+  console.log("Expecting: [1, 2, 3, 4, 5]");
+  console.log("=>", selectionSortRecursive([5, 4, 3, 2, 1]));
 
   console.log("");
 }
 
 module.exports = selectionSortRecursive;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/**
+ * 1. paraphrase
+ * 
+ * use recursion to sort array of nums (least to greatest)
+ * 
+ * 2. test
+ * 
+ * []
+ * [1, 1]
+ * [5, 4, 3, 2, 1]
+ * 
+ * 3. pseudocode
+ * 4. code
+ * 5. refactor
+ * 6. optimize
+ */
